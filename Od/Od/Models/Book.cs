4c5723aa71526a752500ed2344f043fe8abaadc2
4190 <lukasz.dab4@gmail.com>
2023-04-14 19:59:09 +0200

    book model added
    Signed-off-by: 4190 <lukasz.dab4@gmail.com>

--- Od/Od/Models/Book.cs
+++ Od/Od/Models/Book.cs
@@ -1,6 +1,11 @@
-﻿namespace Od.Models
+﻿using Od.Data;
+
+namespace Od.Models
 {
-    public class Book
+    public class Book : IEntity
     {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
     }
 }
