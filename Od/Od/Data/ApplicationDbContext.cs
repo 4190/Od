6365531ee0991785602e22294aaa50bb0b0116d5
4190 <lukasz.dab4@gmail.com>
2023-04-14 20:23:03 +0200

    bewrbasebewb
    Signed-off-by: 4190 <lukasz.dab4@gmail.com>

--- Od/Od/Data/ApplicationDbContext.cs
+++ Od/Od/Data/ApplicationDbContext.cs
@@ -1,6 +1,14 @@
-﻿namespace Od.Data
+﻿using Microsoft.EntityFrameworkCore;
+using Od.Models;
+
+namespace Od.Data
 {
-    public class ApplicationDbContext
+    public class ApplicationDbContext : DbContext
     {
+        public DbSet<Book> Books { get; set; }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
     }
 }
--- Od/Od/Data/IRepository.cs
+++ Od/Od/Data/IRepository.cs
@@ -1,6 +1,11 @@
 ﻿namespace Od.Data
 {
-    public interface IRepository
+    public interface IRepository<T> where T : class, IEntity
     {
+        Task<List<T>> GetAll();
+        Task<T> Get(int id);
+        Task<T> Add(T entity);
+        Task<T> Update(T entity);
+        Task<T> Delete(int id);
     }
 }
