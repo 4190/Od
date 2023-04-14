6f11c2e7b27a4cab858472ee422bb2d8e5039ebe
4190 <lukasz.dab4@gmail.com>
2023-04-14 20:00:29 +0200

    book model was created
    Signed-off-by: 4190 <lukasz.dab4@gmail.com>

--- Od/Od/Models/Book.cs
+++ Od/Od/Models/Book.cs
@@ -1,6 +1,11 @@
-﻿namespace Od.Models
+﻿using Microsoft.Identity.Client;
+
+namespace Od.Models
 {
     public class Book
     {
+        public int IdNumber { get; set; }
+        public string BookTitle { get; set; }
+        public string BookAuthor { get; set; }
     }
 }
